a58c8f66be5d0f59c74bbaf6d3a029abbf1fc6f8
Eduardo Villas Boas <eduardo.villasboas@accountfy.com>
2020-06-28 15:26:37 -0300

    Changing backend message
    Change controller to use abstraction SimpleMessagingTemplate insted use annotation @SendTo

--- websocket-server/src/main/java/villasboas/eduardo/controller/GreetingController.java
+++ websocket-server/src/main/java/villasboas/eduardo/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package villasboas.eduardo.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
@@ -11,13 +11,29 @@ import villasboas.eduardo.websocket.HelloMessage;
 @Controller
 public class GreetingController {
 
-	//Testar SimpMessagingTemplate 
-	
+	private final SimpMessagingTemplate simpMessagingTemplate;
+
+	public GreetingController(SimpMessagingTemplate simpMessagingTemplate) {
+		this.simpMessagingTemplate = simpMessagingTemplate;
+	}
+
 	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws Exception {
-		Thread.sleep(5000);
-		return new Greeting("Hello " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	public void greeting(HelloMessage message) throws Exception {
+		Thread threadToSend = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				simpMessagingTemplate.convertAndSend("/topic/greetings",
+						new Greeting("Hello " + HtmlUtils.htmlEscape(message.getName()) + "!"));
+			}
+
+		});
+		threadToSend.start();
 	}
 
 }
