6763e0870388f46c646c489919a965d9c48c4202
Eduardo Villas Boas <eduardo.villasboas@accountfy.com>
2020-06-28 16:12:48 -0300

    Trace
    Added trace for testing

--- websocket-server/src/main/java/villasboas/eduardo/controller/WebSocketController.java
+++ websocket-server/src/main/java/villasboas/eduardo/controller/WebSocketController.java
@@ -1,5 +1,7 @@
 package villasboas.eduardo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -11,7 +13,14 @@ import villasboas.eduardo.websocket.RequestMessageDto;
 @Controller
 public class WebSocketController {
 
+	private static final int FIVE_MILISECONDS = 5000;
+
+
 	private final SimpMessagingTemplate simpMessagingTemplate;
+	
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketController.class);
+
 
 	public WebSocketController(SimpMessagingTemplate simpMessagingTemplate) {
 		this.simpMessagingTemplate = simpMessagingTemplate;
@@ -19,21 +28,28 @@ public class WebSocketController {
 
 	@MessageMapping("/hello")
 	public void onReceive(RequestMessageDto message) throws Exception {
+		LOGGER.trace("creating thread.");
 		Thread threadToSend = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
+				
 				try {
-					Thread.sleep(5000);
+					LOGGER.trace("Waiting for {} miliseconds", FIVE_MILISECONDS);
+					Thread.sleep(FIVE_MILISECONDS);
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					LOGGER.error("Error when waiting thread for {} miliseconds", FIVE_MILISECONDS);
 				}
+				LOGGER.trace("sending message.");
 				simpMessagingTemplate.convertAndSend("/topic/greetings",
 						new ResponseMessageDto("Hello " + HtmlUtils.htmlEscape(message.getName()) + "!"));
+				LOGGER.trace("message was sent.");
 			}
 
 		});
+		LOGGER.trace("Thread was created {}", threadToSend);
 		threadToSend.start();
+		LOGGER.trace("Thead was started.");
 	}
 
 }
